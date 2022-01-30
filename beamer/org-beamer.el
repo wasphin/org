54327787b55646f12cde7c2f8fa4fd58784f30e5
Xiaofeng Wang <wasphin@gmail.com>
2022-01-30 11:52:53 +0800

    集中自定义主题

--- beamer/org-beamer.el
+++ beamer/org-beamer.el
@@ -1,19 +1,77 @@
 (require 'org)
 (require 'ox)
 (require 'ox-beamer)
+(require 'ox-latex)
 
 (setq
  org-latex-listings 'minted
- org-latex-packages-alist '(("" "minted"))
  org-latex-pdf-process
  '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+   "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
    "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
-
-;; org-latex-export additional packages
-(add-to-list 'org-latex-packages-alist '("" "color" t))
-(add-to-list 'org-latex-packages-alist '("" "listings" t))
-(add-to-list 'org-latex-packages-alist '("" "verbatim" t))
-(add-to-list 'org-latex-packages-alist '("" "zh" t))
 
 (custom-set-variables
  '(org-beamer-outline-frame-title "目录"))
+
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass[10pt, utf8, aspectratio=43, presentation]{beamer}
+
+\\usetheme{Madrid}
+\\usecolortheme{whale}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 中文设置
+\\usepackage[encapsulated]{CJK}
+\\usepackage{CJKutf8, CJKfntef, CJKnumb, CJKpunct, CJKspace}
+\\usepackage{indentfirst}%% useless for beamer
+
+\\newcommand*\\zhdate{%
+  {\\the\\year}~年~{\\the\\month}~月~{\\the\\day}~日}
+\\makeatletter
+\\@ifundefined{today}{\\newcommand\\today\\zhdate}{\\renewcommand\\today\\zhdate}
+\\makeatother
+
+\\AtBeginDocument{\\begin{CJK*}{UTF8}{gbsn}}
+\\AtEndDocument{\\clearpage\\end{CJK*}}
+
+%% 章节前显示目录
+\\AtBeginSection[]{
+  \\begin{frame}<beamer>
+  \\frametitle{目录}
+  \\tableofcontents[currentsection]
+  \\end{frame}
+}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+\\usepackage{color}
+\\usepackage{etoolbox}
+\\usepackage{listings}
+\\usepackage{minted}
+\\usepackage{url}
+\\urlstyle{tt}
+\\usepackage{verbatim}
+
+\\AtEndPreamble{
+  \\hypersetup{%%
+    % bookmarks, % default true
+    % CJKbookmarks,
+    bookmarksdepth     = 3, % to show sections, subsections and subsubsections
+    bookmarksnumbered, % show section counter
+    bookmarksopen      = true, % false by default
+    bookmarksopenlevel = 2, % how many levels to open
+    breaklinks,  % maybe to enable multi-line names
+    % colorlinks,
+    linktocpage, % make page number, not text, be link on TOC, LOC and LOT
+    pdfborder          = 0 0 0,
+    pdfstartpage       = 1,
+    pdfstartview       = FitH,
+    unicode}}
+                [NO-DEFAULT-PACKAGES]
+                [NO-PACKAGES]"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
